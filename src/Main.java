bf56fbefd5f38876e8ea1fbde78c4d2649517cdd
CaptSheep <long.trieu.hr@gmail.com>
2023-08-24 21:11:03 +0700

    Demo KDL C0823L1

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // KDL bien = gtri
+        int number = 1;
+        byte numberByte = 1;
+        float numberFloat = 1.1f;
+        double numberDouble = 1.2;
+        boolean booleanResult = true; //false
+        System.out.println(number);
+        System.out.println(numberByte);
+        System.out.println(numberFloat);
+        System.out.println(numberDouble);
+        System.out.println(booleanResult);
+    }
+}
